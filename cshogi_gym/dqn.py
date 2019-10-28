e9c0efc5d7f77720cd0799a9c98887d65089c49d
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-10-28 22:01:09 +0900

    dqn

--- cshogi_gym/dqn.py
+++ cshogi_gym/dqn.py
@@ -16,8 +16,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-# 入力特徴量：駒の種類×手番 + 持ち駒の種類×手番
-FEATURES_NUM = 14 * 2 + 7 * 2
+# 入力特徴量：駒の種類×手番 + 持ち駒の種類×手番 + 繰り返し数
+FEATURES_NUM = 14 * 2 + 7 * 2 + 1
 
 # 移動の定数
 MOVE_DIRECTION = [
@@ -28,7 +28,8 @@ MOVE_DIRECTION = [
 # 指し手を表すラベルの数
 MAX_MOVE_LABEL_NUM = len(MOVE_DIRECTION) + 7 # 7はhand piece
 
-def make_position_features(board, features):
+def make_position_features(env, features):
+    board = env.board
     # piece
     board.piece_planes_rotate(features)
     pieces_in_hand = board.pieces_in_hand
@@ -45,8 +46,13 @@ def make_position_features(board, features):
                 max_hp_num = 4
 
             features[28 + c * 7 + hp].fill(num / max_hp_num)
+    # 繰り返し数
+    features[42] = (env.repetition_hash[board.zobrist_hash()] - 1) / 3
 
-def make_output_label(move):
+def make_output_label(move, turn):
+    if turn == cshogi.WHITE:
+        move = move_rotate(move)
+
     # 移動先座標
     to_sq = move_to(move)
     # 移動方向((8方向+桂馬2方向)×成2+持ち駒7種類)
@@ -100,7 +106,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Replay Memory
 
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+                        ('state', 'action', 'next_state', 'reward', 'next_actions'))
 
 
 class ReplayMemory(object):
@@ -127,7 +133,7 @@ class ReplayMemory(object):
 ######################################################################
 # DQN
 
-k = 64
+k = 128
 class DQN(nn.Module):
 
     def __init__(self):
@@ -138,30 +144,33 @@ class DQN(nn.Module):
         self.bn2 = nn.BatchNorm2d(k)
         self.conv3 = nn.Conv2d(k, k, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(k)
+        self.conv4 = nn.Conv2d(k, k, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(k)
         self.head = nn.Linear(k * 9 * 9, MAX_MOVE_LABEL_NUM * 9 * 9)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        x = F.relu(self.bn4(self.conv4(x)))
+        return self.head(x.view(x.size(0), -1)).tanh()
 
 
 def get_state(env):
     features = np.zeros((1, FEATURES_NUM, 9, 9), dtype=np.float32)
-    make_position_features(env.board, features[0])
+    make_position_features(env, features[0])
     state = torch.from_numpy(features[:1]).to(device)
     return state
 
 ######################################################################
 # Training
 
-BATCH_SIZE = 128
-GAMMA = 0.999
+BATCH_SIZE = 512
+GAMMA = 0.7
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
-OPTIMIZE_PER_EPISODES = 5
+OPTIMIZE_PER_EPISODES = 2
 TARGET_UPDATE = 10
 
 policy_net = DQN().to(device)
@@ -169,32 +178,57 @@ target_net = DQN().to(device)
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
-optimizer = optim.RMSprop(policy_net.parameters(), lr=1e-3)
+optimizer = optim.RMSprop(policy_net.parameters(), lr=1e-4)
 memory = ReplayMemory(10000)
 
-steps_done = 0
-
-def select_action(board, state):
-    global steps_done
-
+def epsilon_greedy(state, legal_labels, legal_moves):
     sample = random.random()
     eps_threshold = EPS_END + (EPS_START - EPS_END) * \
         math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += 1
+
+    score = 0
+    if sample > eps_threshold:
+        with torch.no_grad():
+            q = policy_net(state)
+            value, select = q[0, legal_labels].max(0)
+            score = int(-math.log(1 / ((torch.clamp(value, -0.99, 0.99).item() + 1) / 2) - 1) * 600)
+    else:
+        select = random.randrange(len(legal_labels))
+    return select, score
+
+temperature = 0.6
+def softmax(state, legal_labels, legal_moves):
+    with torch.no_grad():
+        q = policy_net(state)
+        log_prob = q[0, legal_labels] / temperature
+        select = torch.distributions.categorical.Categorical(logits=log_prob).sample()
+        value = q[0, legal_labels[select]]
+        score = int(-math.log(1 / ((torch.clamp(value, -0.99, 0.99).item() + 1) / 2) - 1) * 600)
+    return select, score
+
+steps_done = 0
+def select_action(board, state):
+    global steps_done
+
+    # 詰み探索
+    if not board.is_check():
+        move = board.mate_move(5)
+        if move != 0:
+            return move, torch.tensor([[make_output_label(move, board.turn)]], device=device, dtype=torch.long), 30000, True
 
     legal_labels = []
     legal_moves = []
     for move in board.legal_moves:
-        legal_labels.append(make_output_label(move))
+        legal_labels.append(make_output_label(move, board.turn))
         legal_moves.append(move)
 
-    if sample > eps_threshold:
-        with torch.no_grad():
-            q = policy_net(state)
-            select = q[0, legal_labels].argmax()
-    else:
-        select = random.randrange(len(legal_labels))
-    return legal_moves[select], torch.tensor([[legal_labels[select]]], device=device, dtype=torch.long)
+    steps_done += 1
+
+
+    select, score = epsilon_greedy(state, legal_labels, legal_moves)
+    #select, score = softmax(state, legal_labels, legal_moves)
+
+    return legal_moves[select], torch.tensor([[legal_labels[select]]], device=device, dtype=torch.long), score, False
 
 
 episode_durations = []
@@ -222,6 +256,13 @@ def optimize_model():
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
+    # 合法手のみ
+    non_final_next_actions_list = []
+    for next_actions in batch.next_actions:
+        if next_actions is not None:
+            non_final_next_actions_list.append(next_actions + [next_actions[0]] * (593 - len(next_actions)))
+    non_final_next_actions = torch.tensor(non_final_next_actions_list, device=device, dtype=torch.long)
+
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken. These are the actions which would've been taken
     # for each batch state according to policy_net
@@ -233,9 +274,12 @@ def optimize_model():
     # This is merged based on the mask, such that we'll have either the expected
     # state value or 0 in case the state was final.
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # 合法手のみの最大値
+    target_q = target_net(non_final_next_states)
+    next_state_values[non_final_mask] = target_q.gather(1, non_final_next_actions).max(1)[0].detach()
     # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    # 相手番の価値のため反転する
+    expected_state_action_values = (-next_state_values * GAMMA) + reward_batch
 
     # Compute Huber loss
     loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
@@ -246,7 +290,7 @@ def optimize_model():
     optimizer.zero_grad()
     loss.backward()
     for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
+        param.grad.data.clamp_(-0.8, 0.8)
     optimizer.step()
 
 
@@ -258,7 +302,7 @@ os.makedirs('kifu', exist_ok=True)
 kif = KIF.Exporter()
 
 num_episodes = 1000
-transitions_per_episode = []
+max_moves = 512
 for i_episode in range(num_episodes):
     # Initialize the environment and state
     env.reset()
@@ -269,19 +313,30 @@ for i_episode in range(num_episodes):
     
     for t in count():
         # Select and perform an action
-        move, action = select_action(env.board, state)
+        move, action, score, mate = select_action(env.board, state)
         _, reward, done, _ = env.step(move)
+
+        #詰みの場合
+        if mate:
+            reward = 1.0
+            done = True
+        # 持将棋の場合
+        if t + 1 == max_moves:
+            done = True
+
         reward = torch.tensor([reward], device=device)
-        #env.render('sfen')
 
         # Observe new state
         if not done:
             next_state = get_state(env)
+            next_actions = [make_output_label(move, env.board.turn) for move in env.board.legal_moves]
         else:
             next_state = None
+            next_actions = None
 
         # 棋譜出力
         kif.move(move)
+        kif.info('info score cp ' + str(score))
         if done:
             if env.is_draw == cshogi.REPETITION_DRAW:
                 kif.end('sennichite')
@@ -289,23 +344,18 @@ for i_episode in range(num_episodes):
                 kif.end('illegal_win')
             elif env.is_draw == cshogi.REPETITION_LOSE:
                 kif.end('illegal_lose')
+            elif t + 1 == max_moves:
+                kif.end('draw')
             else:
-                kif.move('resign')
+                kif.end('resign')
 
-        # 終局まで報酬がわからないため遷移を保存しておく
-        transitions_per_episode.append((state, action, next_state))
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward, next_actions)
 
         # Move to the next state
         state = next_state
 
         if done:
-            # 手番を持っている側からみた報酬に変換
-            reward = reward if t % 2 == 0 else -reward
-            for transition in transitions_per_episode:
-                # Store the transition in memory
-                memory.push(*transition, reward)
-                reward = -reward
-            transitions_per_episode.clear()
             kif.close()
             break
 
@@ -318,5 +368,4 @@ for i_episode in range(num_episodes):
             target_net.load_state_dict(policy_net.state_dict())
 
 print('Complete')
-env.render()
 env.close()
